216b7827d3dc33be74494f97b6d4a76729bc9bbe
mariopopk <travesura-realizada@hotmail.com>
2022-12-18 20:47:50 -0700

    Set up font - bold

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "library";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: "#3a3a3a", minHeight: "100vh" }}>
+      <Button>Play</Button>
+      {/* TODO: Navbar
+  - Logo (C)
+  - Desktop Options
+     - NavBar (C)
+  - Mobile Options
+    - Icon Button (C)
+  - Search
+    - Form (C)
+  - Profile Options
+    - Menu (C)
+      - Menu Options (C)
+
+*/}
+
+      {/* TODO: Jumbotron
+  - Movie or TV Show
+    - Typography (C)
+  - Title or Movie/Show Logo
+    - Typography (C)
+  - Special Text
+    - Typography (C)
+  - Button (C)
+  - Maturity Level
+    - Icon Button (C)
+*/}
+
+      {/* TODO: Movie Carousel
+    - Carousel Title
+     - Typography (C)
+    - Movie Card
+      - Media Card (C)
+
+*/}
     </div>
   );
 }
