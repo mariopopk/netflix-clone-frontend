16bf2d6a444cb45d26f7994e9659a31456167c99
mariopopk <travesura-realizada@hotmail.com>
2023-01-09 14:40:05 -0700

    Deployment setup

---
+++ scripts/heroku-start.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Check if request is from HTTP and if so, redirect to HTTPS
+app.enable("trust proxy");
+app.use((req, res, next) => {
+  req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+});
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+// Root Redirects to the build in assets folder
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "build"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port: ", port);
+});
